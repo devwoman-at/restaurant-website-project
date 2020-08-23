ac94e3e1cc554fbe4624317dc373c1cf32124187
Ana <hi@devwoman.com>
2020-08-23 18:51:19 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const elementsPhone = document.querySelectorAll('.input-phone');
+const maskOptions = {
+  mask: '+{48} 000-000-000',
+};
+elementsPhone.forEach(element => {
+   IMask(element, maskOptions);
+})
+
+const choices = new Choices('.select', {
+   searchEnabled: false,
+});
+
+flatpickr("#date", {
+   dateFormat: 'm/d'
+});
+
+flatpickr("#time", {
+   enableTime: true,
+   noCalendar: true,
+   dateFormat: "H:i",
+   time_24hr: true
+});
+
+var mySwiper = new Swiper ('.swiper-container', {
+   slidesPerView: 1,
+   autoHeight:true
+})
+
+
+const onTabClick = function(idContainer, idElement) {
+   let dataAttribute = `[data-${idContainer}`;
+   for (let item of document.querySelectorAll(`${dataAttribute}-name`)) {
+     item.classList.remove('active');
+   }
+   for (let item of document.querySelectorAll(`${dataAttribute}-tab`)) {
+     item.classList.remove('active');
+   }
+   document.querySelector(`${dataAttribute}-name=${idElement}]`).classList.add('active');
+   document.querySelector(`${dataAttribute}-tab=${idElement}]`).classList.add('active');
+ }
+
+ let tabsName = document.querySelectorAll('.tab-name');
+ tabsName.forEach((tab) => {
+   tab.addEventListener('click', (event) => {
+     console.log(event.target.dataset.menuTabsName)
+     onTabClick(event.target.parentElement.id, event.target.dataset.menuTabsName)
+   })
+ })
+
+ const lightbox = GLightbox({
+  selector: '.gallery-image',
+  touchNavigation: true,
+  loop: true,
+ });
+
+ let
+
+
+
+
+
+
+
+
+
+
+
+
+
+
